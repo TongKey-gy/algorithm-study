4776a56008a4e79dae56181911618120f66b870c
Tongkey-gy <http://github.com/Tongkey-gy.com>
2026-05-18 12:45:18 +0900

    문제 풀이

--- programmers/lv0/031_접미사 배열.py
+++ programmers/lv0/031_접미사 배열.py
@@ -4,10 +4,10 @@
 
 def solution(my_string):
     answer = []
-
+    
     for i in range(len(my_string)):
         answer.append(my_string[i:])
-
+    
     answer.sort()
-
+    
     return answer
